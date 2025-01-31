01da8a9dd21e643c687ca033734c0e1fc9359d31
Arden Roddie <aroddie@mcad.edu>
2025-01-30 19:03:11 -0800

    Fixed grammar and added some easter eggs

--- week2/exercise2-2/games.js
+++ week2/exercise2-2/games.js
@@ -12,7 +12,7 @@ const myGames = [
         type: 'Massively Multiplayer Online Roleplaying Game (MMORPG)',
         graphics: '3D rendered',
         numberOfPlayers: 'millions of players',
-        shortDesc: 'you are a champion of Azeroth, a world plagued by faction wars and existential perils. Your job is to team up with friends and allies to defeat the enemies who threaten your home. You can also explore new lands and make new friends along the way.',
+        shortDesc: 'you are a champion of Azeroth, a world plagued by faction wars and existential perils. Your job is to team up with friends and allies to defeat the enemies who threaten your home. You can also explore new lands, complete quests, and make new friends along the way.',
     },
     {
         name: 'Slay the Princess',
@@ -29,6 +29,19 @@ const myGames = [
 // console.log('This game is ' + myGames[randomIdx].name + '. It is a ' + myGames[randomIdx].type + ' for ' + myGames[randomIdx].numberOfPlayers + '. In this game ' + myGames[randomIdx].shortDesc);
 /** Testing over */
 
-Number.game = window.prompt('I have three games. Please pick a number from 1-3 and I will tell you about a game') - 1;
+Number.game = window.prompt('I have three games. Please pick a number from 1-3 and I will tell you about a game.') - 1;
 
 window.alert('This game is ' + myGames[Number.game].name + '. It is a ' + myGames[Number.game].type + ' with ' + myGames[Number.game].graphics + ' graphics for ' + myGames[Number.game].numberOfPlayers + '. In this game ' + myGames[Number.game].shortDesc);
+
+// Easter Eggs
+if(Number.game === 0) {
+    console.log('Down with JojaCorp!');
+};
+
+if(Number.game === 1) {
+    console.log('For the Horde!');
+};
+
+if(Number.game === 2) {
+    console.log("Slay the Princess? Please don't!");
+};
