1967f9a5850e8c2704f6197ee19168cdea1765bd
Arden Roddie <aroddie@mcad.edu>
2025-02-12 20:59:00 -0800

    added better comments

--- week4/exercise4-1/llamas.js
+++ week4/exercise4-1/llamas.js
@@ -1,12 +1,15 @@
 // select empty div and assign to a variable
 const myEmptyDiv = document.querySelector('#myEmptyDiv');
 
-// put an H1 inside of it
+// create an h1
 const heading = document.createElement('h1');
 heading.innerHTML = 'Llamas communicate by humming!';
-heading.style.cursor = 'pointer';
+heading.style.cursor = 'pointer'; // turns curser into pointing hand, indicating it can be clicked
+// add event listener
 heading.addEventListener('click', handleHeadingClick)
+// Attach div to new heading
 myEmptyDiv.appendChild(heading);
+// create function to make something happen on click
 function handleHeadingClick() {
     document.body.style.backgroundColor = "aquamarine";
 }
